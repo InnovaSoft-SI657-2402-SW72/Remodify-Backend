279401305b91d4abc688fd27777deda21ad857ed
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-09-26 15:56:02 -0500

    fix: change v

--- src/main/java/com/metasoft/restyle/platform/iam/interfaces/rest/transform/SignUpCommandFromResourceAssembler.java
+++ src/main/java/com/metasoft/restyle/platform/iam/interfaces/rest/transform/SignUpCommandFromResourceAssembler.java
@@ -5,14 +5,17 @@ import com.metasoft.restyle.platform.iam.domain.model.entities.Role;
 import com.metasoft.restyle.platform.iam.interfaces.rest.resources.SignUpResource;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SignUpCommandFromResourceAssembler {
     public static SignUpCommand toCommandFromResource(SignUpResource resource) {
-        var roles = resource.roles() != null
+        List<Role> roles = resource.roles() != null
                 ? resource.roles().stream().map(name -> Role.toRoleFromName(name)).toList()
-                : new ArrayList<Role>();
+                : new ArrayList<>();
         System.out.print("Roles:");
-        System.out.println(roles.getFirst().getName().name());
+        if (!roles.isEmpty()) {
+            System.out.println(roles.get(0).getName().name());
+        }
         return new SignUpCommand(resource.username(), resource.password(), roles, resource.email(), resource.firstName(), resource.paternalSurname(), resource.maternalSurname(), resource.description(), resource.phone(), resource.image());
     }
 }
